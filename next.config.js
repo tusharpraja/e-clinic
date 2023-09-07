76e631c5b54fbe4251979f8bfa91969e0ef1bad3
Tushar Prajapati <100289919+tusharpraja@users.noreply.github.com>
2023-09-07 19:32:11 +0530

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    webpack(config) {
+        config.experiments = {
+            asyncWebAssembly: true,
+            layers: true,
+        };
+
+        return config;
+    },
+}
+
+module.exports = nextConfig
